5c0d64a96fb10d72915be379d85ce508d5f6b98d
Ally Jarjour <allyjarjour@gmail.com>
2020-07-19 14:35:19 -0600

    Create boilerplate for PostItem component for routing purposes

---
+++ screens/PostItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text } from "react-native";
+
+
+const PostItem = () => {
+  return (
+    <View>
+      <Text>Post an Item</Text>
+    </View>
+  )
+}
+
+export default PostItem;

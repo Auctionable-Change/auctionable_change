87e797a560bb3810e520d9e365a8c08639cebe2a
Brian Roxbury <roxburybrian@gmail.com>
2020-07-19 13:50:04 -0600

    Add Scroll feature to current listings page, and mock out data and data file

---
+++ mockData/mockListings.js
@@ -0,0 +1,30 @@
+const mockListings = [
+  {
+    name: "BBQ Grill",
+    price: 50,
+    imageSource: "./grill.jpg",
+    category: 'Home'
+  },
+  {
+    name: "Stroller",
+    price: 50,
+    imageSource: "./grill.jpg",
+    category: 'Baby/Kids'  
+  },
+  {
+    name: "Coffee Table",
+    price: 50,
+    imageSource: "./grill.jpg",
+    category: 'Furniture'  
+  },
+  {
+    name: "Fridge",
+    price: 50,
+    imageSource: "./grill.jpg",
+    category: 'Home'  
+  }
+
+]
+
+export default mockListings
+  
--- screens/CurrentListings.js
+++ screens/CurrentListings.js
@@ -1,16 +1,58 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, Image, StyleSheet, ScrollView } from "react-native";
+import mockListings from '../mockData/mockListings'
 
 const CurrentListings = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Current Listings</Text>
-      <Button
-        title="Listing Details"
-        onPress={() => navigation.navigate("Listing Details")}
-      />
+    <ScrollView>
+      { 
+        mockListings.map((listing) => {
+          return (
+            <View key={listing.name} style={styles.container}>
+              <Text style={styles.pageTitle}>{listing.name}</Text>
+              
+              <Image source={ require('./grill.jpg') } 
+                     style={ styles.image }
+              />
+              <Text>{`Current Price: ${listing.price}`}</Text>
+              <Button
+                title="Listing Details"
+                onPress={() => navigation.navigate("Listing Details")}
+              />
+            </View>
+          )
+        })
+      }
+    </ScrollView>
+     
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+   
+  },
+  image: {
+    width: 250,
+    height: 250,
+    resizeMode: 'stretch'
+
+  },
+  pageTitle: {
+    marginBottom: 5,
+    fontSize: 25,
+    justifyContent: "center"
+  },
+  listing: {
+    marginTop: 5,
+    padding: 20
+  }
+})
 
 export default CurrentListings;

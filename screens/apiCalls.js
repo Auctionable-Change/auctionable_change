9393d7c694c60f39ea172eca77a141ea6a087926
Brian Roxbury <roxburybrian@gmail.com>
2020-07-22 12:25:33 -0600

    Connect cloudinary

--- screens/apiCalls.js
+++ screens/apiCalls.js
@@ -12,3 +12,30 @@ export const fetchItems = async () => {
     return false
   }
 }
+
+let CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/djk5anakm/upload';
+
+
+export const cloudinaryPost = (pickerResult) => {
+  console.log('pickerResult', pickerResult)
+  let base64Img = `data:image/jpg;base64,${pickerResult.base64}`;
+
+  let data = {
+    "file": base64Img,
+    "upload_preset": "oclsdjxp",
+  }
+
+  fetch(CLOUDINARY_URL, {
+    body: JSON.stringify(data),
+    headers: {
+      'content-type': 'application/json'
+    },
+    method: 'POST',
+  }).then(async r => {
+    let data = await r.json()
+    console.log('from apicalls', data)
+    return data.url;
+  }).catch(err => console.log(err))
+}
+
+

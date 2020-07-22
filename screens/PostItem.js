cbeef96cf8936934af87090a34ab38e74593b8ea
“Lauren <lucerolaurenm@gmail.com>
2020-07-22 15:51:32 -0400

    Add input style and image upload

--- screens/PostItem.js
+++ screens/PostItem.js
@@ -1,33 +1,82 @@
-import * as React from "react";
-import { SafeAreaView, Text, TextInput } from "react-native";
-import { StyleSheet } from "react-native";
-import { TouchableOpacity, ScrollView } from "react-native-gesture-handler";
-import { useStore } from "../store";
+import React, { useState, useEffect } from "react";
+import { Button, Image, Text, TextInput, StyleSheet } from "react-native";
+import {
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  ScrollView,
+} from "react-native-gesture-handler";
+import { SafeAreaView } from "react-native-safe-area-context";
+import * as ImagePicker from "expo-image-picker";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import { cloudinaryPost } from "./apiCalls";
 
 const PostItem = ({ navigation }) => {
+  const [image, uploadImage] = useState(null);
+  const [imageObj, setImageObj] = useState(null);
+
+  useEffect(() => {
+    getPermissionAsync();
+  });
+
+  const getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
+    }
+  };
+
+  const _pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Image,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+        base64: true,
+      });
+      if (!result.cancelled) {
+        uploadImage(result.uri);
+        setImageObj(result);
+      }
+    } catch (E) {
+      console.log(E);
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <Text>Post an Item</Text>
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          placeholder="Your Name"
+      <ScrollView contentContainerStyle={styles.scroll}>
+        <Text style={styles.title}>Post an Item</Text>
+        <TextInput style={styles.textInput} 
+        accessibilityLabel="Your Name"
+        autoCapitalize="words"
+        autoCompleteType="name" />
+        <TextInput style={styles.textInput} placeholder="Your Email"
+        autoCompleteType="email" />
+        <TextInput style={styles.textInput} placeholder="Item Name" />
+        <Button
+          title="Pick an image from camera roll"
+          color="#2cb833"
+          onPress={_pickImage}
         />
+        {image ? (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        ) : (
+          <TouchableWithoutFeedback onPress={_pickImage}>
+            <Image
+              source={require("../assets/icons/camera.png")}
+              style={{ width: 150, height: 150 }}
+            />
+          </TouchableWithoutFeedback>
+        )}
+        <TextInput style={styles.textInput} placeholder="Item Description" />
         <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          placeholder="Your Email"
-        />
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          placeholder="Name of Item"
-        />
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-          placeholder="Item Description"
-        />
-        <TextInput
-          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          style={styles.textInput}
           placeholder="Minimum Donation for Item"
+          keyboardType="numeric"
         />
         <TouchableOpacity
           onPress={() => navigation.navigate("Choose Charity")}
@@ -43,8 +92,25 @@ const PostItem = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
+  },
+  scroll: {
+    display: "flex",
     alignItems: "center",
-    justifyContent: "center",
+  },
+  title: {
+    fontSize: 25,
+    textAlign: "center",
+    margin: 20,
+  },
+  textInput: {
+    height: 30,
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+    borderRadius: 10,
+    padding: 5,
+    width: 250,
   },
   button: {
     backgroundColor: "#FFFFFF",

a1682f7489d693060f55b291859a7d6bdf3fff48
Brian Roxbury <roxburybrian@gmail.com>
2020-07-24 09:55:56 -0600

    Fix merge conflict in postConfirmation

--- screens/PostConfirmation.js
+++ screens/PostConfirmation.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { SafeAreaView, Text } from "react-native";
 import { StyleSheet } from "react-native";
 import { TouchableOpacity, ScrollView } from "react-native-gesture-handler";
+import { useStore } from '../store'
 
 const PostConfirmation = ({ navigation }) => {
   const { state } = useStore()

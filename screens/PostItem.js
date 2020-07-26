f7daa29aae30a89bfb2eb1ae3717bd5527e20c0b
Ally Jarjour <allyjarjour@gmail.com>
2020-07-25 21:50:20 -0600

    Add padding around form in PostItem screen

--- screens/PostItem.js
+++ screens/PostItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import SellerForm from "../components/SellerForm";
 import NavBar from "../components/NavBar/NavBar";
@@ -8,8 +8,10 @@ import NavBar from "../components/NavBar/NavBar";
 const PostItem = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
-      <Text style={styles.title}>Post an Item</Text>
-      <SellerForm style={styles.form} navigation={navigation} />
+      <View style={styles.container}>
+        <Text style={styles.title}>Post an Item</Text>
+        <SellerForm style={styles.form} navigation={navigation} />
+      </View>
       <NavBar navigation={navigation} />
     </SafeAreaView>
   );
@@ -20,6 +22,11 @@ const styles = StyleSheet.create({
     fontSize: 25,
     textAlign: "center",
     marginBottom: 20,
+  },
+  container: {
+    flex: 1,
+    paddingLeft: 30,
+    paddingRight: 30,
   },
 });
 

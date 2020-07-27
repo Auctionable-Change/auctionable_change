4c4fb653bae64f196a85500cb11d4131392241da
“Lauren <lucerolaurenm@gmail.com>
2020-07-27 15:51:29 -0400

    Update Thank You text and styles

--- screens/ThankYou/ThankYou.js
+++ screens/ThankYou/ThankYou.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import NavBar from "../../components/NavBar/NavBar";
 
@@ -7,11 +7,16 @@ const ThankYou = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View>
-        <Text>Thank you for contributing to A.Change!</Text>
+        <Text style={styles.text}>Thank you for contributing to A.change!</Text>
+        <Text style={styles.text}>Please check your email for updates.</Text>
       </View>
       <NavBar navigation={navigation} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  text: { fontSize: 20, margin: 5 },
+});
 
 export default ThankYou;

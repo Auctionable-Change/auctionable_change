b9f2a206d1d73189541295307f58d15a71237052
“Lauren <lucerolaurenm@gmail.com>
2020-07-28 10:48:54 -0400

    Adjust margins and remove scroll view

--- screens/PaymentInstructions/PaymentInstructions.js
+++ screens/PaymentInstructions/PaymentInstructions.js
@@ -7,50 +7,48 @@ import * as Linking from "expo-linking";
 import Camera from "../Camera/Camera";
 import { ScrollView } from "react-native-gesture-handler";
 
-
 const PaymentInstructions = () => {
   const { state } = useStore();
-  // had to add scroll view because when i changed font the button was below screen
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView
+      {/* <View
         contentContainerStyle={{ alignItems: "center", margin: 0 }}
-      >
-        <Text style={styles.title}>Thank you for your contribution!</Text>
-        <Text style={styles.subtitle}>Instructions to complete order:</Text>
-        <View style={styles.stepOne}>
-          <Text style={{ fontFamily: "quicksand", fontSize: 15 }}>
-            First, follow this{" "}
-          </Text>
-          <Text
-            style={{
-              fontSize: 15,
-              fontFamily: "quicksand-bold",
-              textDecorationLine: "underline",
-            }}
-            onPress={() => Linking.openURL(state.currentListing.charity_url)}
-          >
-            link{" "}
-          </Text>
-          <Text style={{ fontFamily: "quicksand", fontSize: 15 }}>
-            to donate at least ${state.currentListing.price}.
-          </Text>
-        </View>
-        <Text style={styles.listItem}>
-          Then, take a screenshot that includes your name, charity name and
-          donation amount.
+      > */}
+      <Text style={styles.title}>Thank you for your contribution!</Text>
+      <Text style={styles.subtitle}>Instructions to complete order:</Text>
+      <View style={styles.stepOne}>
+        <Text style={{ fontFamily: "quicksand", fontSize: 15 }}>
+          First, follow this{" "}
         </Text>
-        <Text style={styles.listItem}>
-          Do not include other personal information.
+        <Text
+          style={{
+            fontSize: 15,
+            fontFamily: "quicksand-bold",
+            textDecorationLine: "underline",
+          }}
+          onPress={() => Linking.openURL(state.currentListing.charity_url)}
+        >
+          link{" "}
         </Text>
-        <Camera
-          cameraType="launchImageLibraryAsync"
-          user="buyer"
-          prompt="Press camera to upload screenshot of donation receipt from your camera roll."
-          title="Upload Receipt"
-        />
-      </ScrollView>
+        <Text style={{ fontFamily: "quicksand", fontSize: 15 }}>
+          to donate at least ${state.currentListing.price}.
+        </Text>
+      </View>
+      <Text style={styles.listItem}>
+        Then, take a screenshot that includes your name, charity name and
+        donation amount.
+      </Text>
+      <Text style={styles.listItem}>
+        Do not include other personal information.
+      </Text>
+      <Camera
+        cameraType="launchImageLibraryAsync"
+        user="buyer"
+        prompt="Press camera to upload screenshot of donation receipt from your camera roll."
+        title="Upload Receipt"
+      />
+      {/* </View> */}
     </SafeAreaView>
   );
 };
@@ -60,21 +58,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#FFFFFF",
     display: "flex",
+    alignItems: "center",
   },
   subtitle: {
     fontSize: 20,
     fontFamily: "quicksand",
-    margin: 10,
+    margin: 5,
   },
   title: {
     fontSize: 25,
     textAlign: "center",
-    margin: 10,
+    margin: 5,
     fontFamily: "quicksand-bold",
   },
   listItem: {
     fontSize: 15,
-    margin: 20,
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 5,
+    marginBottom: 5,
     fontFamily: "quicksand",
   },
   stepOne: {

63b680433dd3d750fa28ca466c2a0dfeac894223
Brian Roxbury <roxburybrian@gmail.com>
2020-08-12 13:36:47 -0600

    Attempt to display bid history on profile page

--- screens/Profile/Profile.js
+++ screens/Profile/Profile.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { StyleSheet, FlatList } from "react-native";
-import { Button, Text, Segment } from 'native-base'
+import { Button, Text, Segment, Card, View } from 'native-base'
 import { SafeAreaView } from "react-native-safe-area-context";
 import NavBar from "../../components/NavBar/NavBar";
 import { fetchUserInfo, fetchBid} from "../apiCalls";
@@ -12,35 +12,50 @@ const Profile = ({ navigation }) => {
   const [bidHistory, setBidHistory] = useState([])
   const [activePage, setActivePage] = useState(1)
 
-  const segmentHandler = async (page) => {
-  
-    setActivePage(page)
-    if(activePage === 1) {
-      fetchUserInfo(1)
-        .then(data => {
-          const promises = data.map(bid => {
-            return fetchBid(bid.item_id)
-            .then(info => {
-              return {
-                itemName: info.title,
-                auctionEnd: info.auction_end,
-                charity: info.charity,
-                bidAmount: bid.amount,
-              }
-            })
-          })
-          Promise.all(promises).then(bidsInfo => setBidHistory(bidsInfo))
+  const getBidData = async (userId) => {
+    fetchUserInfo(userId)
+    .then(data => {
+      const promises = data.map(bid => {
+        return fetchBid(bid.item_id)
+        .then(info => {
+          return {
+            itemName: info.title,
+            auctionEnd: info.auction_end,
+            charity: info.charity,
+            bidAmount: bid.amount,
+            id: bid.id,
+            winner: bid.winner,
+          }
+        })
       })
-      console.log('bidHistory', bidHistory)
-    }
-     
-      return (
-        <FlatList data={userBids}>
-          
-        </FlatList>
-      )
-     
+    Promise.all(promises).then(bidsInfo => setBidHistory(bidsInfo))
+  })
   }
+
+  const segmentHandler = async (page) => {
+    setActivePage(page)
+    await getBidData(1)
+
+    if(page === 1) {
+      let timestampNow = Math.floor(new Date().getTime()/1000.0)
+      // console.log('bidauctionend', bid.auctionEnd)
+      console.log('timestampNow', timestampNow)
+
+      let filteredBids = bidHistory.filter(bid => {
+        console.log('bidauctionend', bid.auctionEnd)
+        (bid.auctionEnd - timestampNow) > 0
+      })
+      setBidHistory(filteredBids)
+    }
+
+    if(page === 2) {
+      setBidHistory(bidHistory)
+    }
+  }
+
+  useEffect( () => {
+    segmentHandler(1)
+  }, [])
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
@@ -53,6 +68,17 @@ const Profile = ({ navigation }) => {
           <Text>Bid History</Text>
         </Button>
       </Segment>
+        <FlatList data={bidHistory}
+                    style={styles.scrollView}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                      <Card style={styles.cardDefault}>
+                        <View>
+                          <Text>Item Title: {item.itemName}</Text>
+                        </View>
+                      </Card>
+                    )}
+          />
       <NavBar navigation={navigation} />
     </SafeAreaView>
   );
@@ -69,6 +95,18 @@ const styles = StyleSheet.create({
     fontFamily: "quicksand",
     color: "#4a4a4a",
     textAlign: "center"
+  },
+  scrollView: {
+    flex: 1,
+    width: "90%",
+  },
+  cardDefault: {
+    backgroundColor: "#FFF",
+    padding: 15,
+    width: "98%",
+    marginTop: 15,
+    height: 130,
+    flexDirection: "row",
   },
 });
 
